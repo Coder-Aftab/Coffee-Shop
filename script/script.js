e232aa68daa73e14ba390a25d273f8c930c3853b
Md Aftab <mda552669@gmail.com>
2022-10-24 22:01:51 +0530

    add to cart sending raw data

--- script/script.js
+++ script/script.js
@@ -1,69 +1,5 @@
 import { data } from "./data.js";
 
-const res = document.querySelector(".root");
-
-const _arr = [];
-class Shop {
-  constructor() {}
-
-  addOrder(obj) {
-    _arr.push(obj);
-  }
-  showDetails() {
-    return _arr;
-  }
-}
-
-class User extends Shop {
-  constructor() {
-    super();
-  }
-  addProd(prod, ...addOn) {
-    const obj = {
-      prod,
-      addOn,
-    };
-    super.addOrder(obj);
-  }
-}
-
-class Coffee {
-  constructor(prod, addOn, qty) {
-    this.coffee = { prod, addOn, qty };
-  }
-  Espresso() {}
-
-  Cappuccino() {
-    this.la.cream = cream;
-    this.la.amount += 125;
-  }
-  Latte() {
-    this.la.latte = latte;
-    this.la.amount += 150;
-  }
-  addQty(val) {
-    this.es.qty = this.es.qty + val;
-  }
-  resetQty(val) {
-    this.es.qty = val;
-  }
-}
-
-const newUser = new User();
-
-newUser.addProd("Es", "Milk", "Latte");
-
-newUser.addProd("Cap", "Milk", "Latte");
-const orderDetails = newUser.showDetails();
-
-console.log(orderDetails);
-const showDom = orderDetails.reduce((acc, cur) => {
-  acc += `<span style='font-weight:bold'>${cur["prod"]}</span>
-          ${cur["addOn"]}
-          `;
-  return acc;
-}, ``);
-
 // Cards
 
 const card = document.querySelector(".product--card");
@@ -71,36 +7,47 @@ const card = document.querySelector(".product--card");
 const CardItems = data.map((item) => {
   const node = document.createElement("section");
   node.innerHTML = `
-  <section class="card card--col card--secondary card--outline">
+  <form class="card card--col card--secondary card--outline ${
+    item.name
+  } form" id=${item.id}>
     <img class="card__img" src=${item.img} alt="espresso">
     <div class="addOn addOn--primary addOn--label"  id=${item.id}>
       <header class="addOn__header">
         <h3 class="addOn__heading">Milk<span> + $${item.milk}</span></h3>
       </header>
-     <button class="icon icon--small toggle-switch-of" value="milk"></button>
+      <input class="checkBox" name="latte" type="checkbox" id=${
+        Date.now() + 10
+      }>
+    <label class="checkBox__label" for=${Date.now() + 10}></label>
     </div>
     <div class="addOn addOn--primary addOn--label" id=${item.id}>
       <header class="addOn__header">
         <h3 class="addOn__heading">Cream<span> + $${item.cream}</span></h3>
       </header>
-      <button class="icon icon--small toggle-switch-of" value="cream"></button>
+      <input class="checkBox" name="cream" type="checkbox" id=${
+        Date.now() + 20
+      }>
+    <label class="checkBox__label" for=${Date.now() + 20}></label>
     </div>
     <div class="addOn addOn--primary addOn--label" id=${item.id}>
       <header class="addOn__header">
         <h3 class="addOn__heading">Latte<span> + $${item.latte}</span></h3>
       </header>
-     <button class="icon icon--small toggle-switch-of" value="latte"></button>
+     <input class="checkBox" name="latte" type="checkbox" id=${Date.now() + 35}>
+    <label class="checkBox__label" for=${Date.now() + 35}></label>
     </div>
     <div class="addOn__grid">
       <div class="addOn addOn--primary" id=${item.id}>
         <h3 class="addOn__heading">QTY</h3>
-        <button class="btn btn--primary qty-btn" type="button" value='-'>-</button>
+        <button class="btn btn--primary qty-btn" type="button" value='-' type="button">-</button>
         <span class="qty">1</span><button class="btn btn--primary qty-btn" type="button" value='+'>+</button>
       </div>
-      <button class="addOn addOn--tertiary addOn__cart-btn" id=${item.id}>Add To Cart
+      <button class="addOn addOn--tertiary addOn__cart-btn" id=${
+        item.id
+      }>Add To Cart
       </button>
     </div>
-  </section>`;
+  </form>`;
   return node;
 });
 
@@ -108,7 +55,6 @@ CardItems.forEach((section) => {
   card.appendChild(section);
 });
 
-const addToCartBtns = document.querySelectorAll(".addOn__cart-btn");
 const qtys = document.querySelectorAll(".qty");
 const qtyBtns = document.querySelectorAll(".qty-btn");
 qtyBtns.forEach((qtyBtn) => {
@@ -151,52 +97,129 @@ function handleLatte(e) {
   }
 }
 
-addToCartBtns.forEach((btn) => {
-  btn.onclick = (e) => {
-    handleAddToCart(e);
+let BillDetails = [];
+
+function handleAddToCart(val, id) {
+  const newItem = {
+    id: Date.now(),
+    parentId: id,
+    milk: val[0],
+    cream: val[1],
+    latte: val[2],
+    qty: Number(qtys[id - 1].innerText),
+  };
+  qtys[id - 1].innerText = 1;
+  console.log(newItem);
+}
+
+function renderBill(billItem) {
+  
+}
+
+const forms = document.querySelectorAll(".form");
+
+forms.forEach((form) => {
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    const val = Object.values(form).map((val) => val.checked);
+    handleAddToCart(val, form.id);
+    form.reset();
   };
 });
 
-let BillDetails = [];
+// card.addEventListener("click", handleCard);
 
-function handleAddToCart(e) {
-  const currentEvent = e.target;
-  console.log(qtys[currentEvent.id-1]);
-  const newItem = {
-    id: Date.now(),
-    parentId: currentEvent.id,
-    milk: false,
-    cream:false,
-    latte: false,
-    qty:Number(qtys[currentEvent.id-1].innerText)
-  }
-  console.log(newItem)
-}
+// function handleCard(e) {
+//   if (e.target.classList.contains("toggle-switch-of")) {
+//     const value = e.target.value;
+//     console.log(e.target.parentElement.id);
+//     handleSwitch(e, value, e.target.parentElement.id);
+//   }
+// }
 
+// function handleSwitch(e, value, id) {
+//   if ((value == "milk" || value == "cream" || value == "latte") && id == 1) {
+//     e.target.classList.toggle("toggle-switch-on");
+//   } else if (
+//     (value == "milk" || value == "cream" || value == "latte") &&
+//     id == 2
+//   ) {
+//     e.target.classList.toggle("toggle-switch-on");
+//   } else if (
+//     (value == "milk" || value == "cream" || value == "latte") &&
+//     id == 3
+//   ) {
+//     e.target.classList.toggle("toggle-switch-on");
+//   }
+// }
 
+// forms
+// const esp = document.querySelector(".espresso");
 
-card.addEventListener("click", handleCard);
+// const cap = document.querySelector(".cappuccino");
 
-function handleCard(e) {
-  if (e.target.classList.contains("toggle-switch-of")) {
-    const value = e.target.value;
-    console.log(e.target.parentElement.id);
-    handleSwitch(e, value, e.target.parentElement.id);
-  }
-}
+// const la=document.querySelector()
 
-function handleSwitch(e, value, id) {
-  if ((value == "milk" || value == "cream" || value == "latte") && id == 1) {
-    e.target.classList.toggle("toggle-switch-on");
-  } else if (
-    (value == "milk" || value == "cream" || value == "latte") &&
-    id == 2
-  ) {
-    e.target.classList.toggle("toggle-switch-on");
-  } else if (
-    (value == "milk" || value == "cream" || value == "latte") &&
-    id == 3
-  ) {
-    e.target.classList.toggle("toggle-switch-on");
-  }
-}
+// const res = document.querySelector(".root");
+
+// const _arr = [];
+// class Shop {
+//   constructor() {}
+
+//   addOrder(obj) {
+//     _arr.push(obj);
+//   }
+//   showDetails() {
+//     return _arr;
+//   }
+// }
+
+// class User extends Shop {
+//   constructor() {
+//     super();
+//   }
+//   addProd(prod, ...addOn) {
+//     const obj = {
+//       prod,
+//       addOn,
+//     };
+//     super.addOrder(obj);
+//   }
+// }
+
+// class Coffee {
+//   constructor(prod, addOn, qty) {
+//     this.coffee = { prod, addOn, qty };
+//   }
+//   Espresso() {}
+
+//   Cappuccino() {
+//     this.la.cream = cream;
+//     this.la.amount += 125;
+//   }
+//   Latte() {
+//     this.la.latte = latte;
+//     this.la.amount += 150;
+//   }
+//   addQty(val) {
+//     this.es.qty = this.es.qty + val;
+//   }
+//   resetQty(val) {
+//     this.es.qty = val;
+//   }
+// }
+
+// const newUser = new User();
+
+// newUser.addProd("Es", "Milk", "Latte");
+
+// newUser.addProd("Cap", "Milk", "Latte");
+// const orderDetails = newUser.showDetails();
+
+// console.log(orderDetails);
+// const showDom = orderDetails.reduce((acc, cur) => {
+//   acc += `<span style='font-weight:bold'>${cur["prod"]}</span>
+//           ${cur["addOn"]}
+//           `;
+//   return acc;
+// }, ``);

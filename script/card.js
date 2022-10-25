afb49cfb97dcd7a76b53385acb1a5a38d8f247f7
Md Aftab <mda552669@gmail.com>
2022-10-26 00:25:43 +0530

    fixed bug --wrong checking of addons

--- script/card.js
+++ script/card.js
@@ -1,5 +1,5 @@
 export const card = ({ id, name, milk, cream, latte, img }) => {
-  const node = document.createElement("section")
+  const node = document.createElement("section");
   node.innerHTML = `
   <form class="card card--col card--secondary card--outline ${name} form" id=${id}>
     <img class="card__img" src=${img} alt=${name}  >
@@ -7,22 +7,24 @@ export const card = ({ id, name, milk, cream, latte, img }) => {
       <header class="addOn__header">
         <h3 class="addOn__heading">Milk<span> + $${milk}</span></h3>
       </header>
-      <input class="checkBox" name="latte" type="checkbox" id=${Date.now()} checked>
-    <label class="checkBox__label" for=${Date.now()}></label>
+      <input class="checkBox" name="milk" type="checkbox" id=${
+        name + "milk"
+      } checked>
+    <label class="checkBox__label" for=${name + "milk"}></label>
     </div>
     <div class="addOn addOn--primary addOn--label" id=${id}>
       <header class="addOn__header">
         <h3 class="addOn__heading">Cream<span> + $${cream}</span></h3>
       </header>
-      <input class="checkBox" name="cream" type="checkbox" id=${Date.now() * 5}>
-    <label class="checkBox__label" for=${Date.now() * 5}></label>
+      <input class="checkBox" name="cream" type="checkbox" id=${name + "cream"}>
+    <label class="checkBox__label" for=${name + "cream"}></label>
     </div>
     <div class="addOn addOn--primary addOn--label" id=${id}>
       <header class="addOn__header">
         <h3 class="addOn__heading">Latte<span> + $${latte}</span></h3>
       </header>
-     <input class="checkBox" name="latte" type="checkbox" id=${Date.now() * 10}>
-    <label class="checkBox__label" for=${Date.now() * 10}></label>
+     <input class="checkBox" name="latte" type="checkbox" id=${name + "latte"}>
+    <label class="checkBox__label" for=${name + "latte"}></label>
     </div>
     <div class="addOn__grid">
       <div class="addOn addOn--primary" id=${id}>
